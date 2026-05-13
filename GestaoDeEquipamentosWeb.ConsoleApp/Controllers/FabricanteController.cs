cd5565a34437663cc10a14655ac322e65971fc64
Kauan Silva <kauanmacedo338@gmail.com>
2026-05-13 19:50:35 -0300

    adicionado CRUD do equipamentos

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -0,0 +1,98 @@
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
+using GestaoDeEquipamentosWeb.ConsoleApp.ModuloFabricante;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private readonly IRepositorio<Fabricante> repositorioFabricante;
+        public FabricanteController()
+        {
+            ContextoJson contextoJson = new ContextoJson();
+            contextoJson.Carregar();
+
+            repositorioFabricante =
+                  new RepositorioFabricanteEmArquivo(contextoJson);
+        }
+
+        [HttpGet]
+
+        public ActionResult Listar()
+        {
+            List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
+
+            return View(fabricantes);
+        }
+
+        [HttpGet]
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Cadastrar(string nome, string email, string telefone)
+        {
+            Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+
+            repositorioFabricante.Cadastrar(novoFabricante);
+
+            return RedirectToAction(nameof(Listar));
+        }
+
+        [HttpGet]
+
+        public ActionResult Editar(string id)
+        {
+            Fabricante? fabricante = repositorioFabricante.SelecionarPorId(id);
+
+            if (fabricante == null)
+                return RedirectToAction(nameof(Listar));
+
+            return View(fabricante);
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Editar(string id, string nome, string email, string telefone)
+        {
+            Fabricante fabricanteAtualizado = new Fabricante(nome, email, telefone);
+
+            repositorioFabricante.Editar(id, fabricanteAtualizado);
+
+            return RedirectToAction(nameof(Listar));
+        }
+
+        [HttpGet]
+
+        public ActionResult Excluir(string id)
+        {
+            Fabricante? fabricante = repositorioFabricante.SelecionarPorId(id);
+
+            if (fabricante == null)
+                return RedirectToAction(nameof(Listar));
+
+            return View(fabricante);
+        }
+
+        [HttpPost]
+        [ActionName("Excluir")]
+
+        public ActionResult ExcluirConfirmado(string id)
+        {
+            Fabricante? fabricante = repositorioFabricante.SelecionarPorId(id);
+            
+            if (fabricante == null)
+                return RedirectToAction(nameof(Listar));
+
+            repositorioFabricante.Excluir(fabricante);
+
+            return RedirectToAction(nameof(Listar));
+        }
+    }
+}
--- GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+app.Run();
