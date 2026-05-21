0be0c9cb901e6661f9c2a96f3a253da335e78610
Kauan Silva <kauanmacedo338@gmail.com>
2026-05-21 00:41:15 -0300

    adicionado Crud de chamados

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/ChamadoController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/ChamadoController.cs
@@ -4,6 +4,7 @@ using GestaoDeEquipamentosWeb.ConsoleApp.Models;
 using GestaoDeEquipamentosWeb.ConsoleApp.ModuloChamado;
 using GestaoDeEquipamentosWeb.ConsoleApp.ModuloEquipamento;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 public class ChamadoController : Controller
 {
@@ -40,6 +41,159 @@ public class ChamadoController : Controller
             visualizarChamados.Add(listarChamadoVm);
         }
 
-        return View();
+        return View(visualizarChamados);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        ViewBag.Equipamentos = CarregarEquipamentos();
+
+        CadastrarChamadosViewModel cadastrarVm = new CadastrarChamadosViewModel(string.Empty, null, string.Empty);
+
+        return View(cadastrarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarChamadosViewModel cadastrarVm)
+    {
+        Equipamento? equipamento =
+            repositorioEquipamento.SelecionarPorId(cadastrarVm.EquipamentoId);
+
+        if (equipamento == null)
+        {
+
+            ModelState.AddModelError(
+                nameof(cadastrarVm.EquipamentoId),
+                "Selecione um equipamento válido"
+            );
+        }
+
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Equipamentos = CarregarEquipamentos();
+
+            return View(cadastrarVm);
+        }
+
+        Chamado novoChamado = new Chamado(
+            cadastrarVm.Titulo,
+            equipamento,
+            cadastrarVm.Descricao
+        );
+
+        repositorioChamado.Cadastrar(novoChamado);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Editar(string id)
+    {
+
+        Chamado? chamado = repositorioChamado.SelecionarPorId(id);
+
+        if (chamado == null)
+            return RedirectToAction(nameof(Listar));
+
+        EditarChamadoViewModel editarVm = new EditarChamadoViewModel(
+                  chamado.Id,
+                  chamado.Titulo,
+                  chamado.Descricao,
+                  chamado.Equipamento.Id
+              );
+
+        ViewBag.Equipamentos = CarregarEquipamentos();
+
+        return View(editarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(EditarChamadoViewModel editarVm)
+    {
+
+        Equipamento? equipamento =
+             repositorioEquipamento.SelecionarPorId(editarVm.EquipamentoId);
+
+        if (equipamento == null)
+        {
+
+            ModelState.AddModelError(
+                nameof(editarVm.EquipamentoId),
+                "Selecione um equipamento válido"
+            );
+        }
+
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Equipamentos = CarregarEquipamentos();
+
+            return View(editarVm);
+        }
+
+        Chamado chamadoEditado = new Chamado(
+            editarVm.Titulo,
+            equipamento,
+            editarVm.Descricao
+        );
+
+        repositorioChamado.Editar(editarVm.Id, chamadoEditado);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(string id)
+    {
+
+        Chamado? chamado = repositorioChamado.SelecionarPorId(id);
+
+        if (chamado == null)
+            return RedirectToAction(nameof(Listar));
+
+
+        ExcluirChamadoViewModel excluirVm = new ExcluirChamadoViewModel(
+            chamado.Id,
+            chamado.Titulo,
+            chamado.Descricao,
+            chamado.Equipamento.Nome,
+            chamado.DataAbertura,
+            chamado.TempoDecorrido,
+            chamado.EstaConcluido
+        );
+
+        return View(excluirVm);
+    }
+
+    [HttpPost]
+    [ActionName("Excluir")]
+
+    public ActionResult ExcluirConfirmado(ExcluirChamadoViewModel excluirVm)
+    {
+        Chamado? chamado = repositorioChamado.SelecionarPorId(excluirVm.Id);
+
+        if (chamado != null)
+           repositorioChamado.Excluir(chamado);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    private List<SelectListItem> CarregarEquipamentos()
+    {
+        List<Equipamento> equipamentos = repositorioEquipamento.SelecionarTodos();
+
+        List<SelectListItem> selecionarEquipamentos = new List<SelectListItem>();
+
+        foreach (Equipamento e in equipamentos)
+        {
+            SelectListItem selecionarEquipamentoVm = new SelectListItem(
+                e.Nome,
+                e.Id
+            );
+
+            selecionarEquipamentos.Add(selecionarEquipamentoVm);
+        }
+
+        return selecionarEquipamentos;
     }
 }
--- GestaoDeEquipamentosWeb.ConsoleApp/Models/ChamadoViewModel.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Models/ChamadoViewModel.cs
@@ -1,8 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http.Timeouts;
+
 namespace GestaoDeEquipamentosWeb.ConsoleApp.Models;
 
-public record ListarChamadoViewModel (
+public record ListarChamadoViewModel(
     string Id,
-    string Titulo, 
+    string Titulo,
+    string Equipamento,
+    DateTime DataAbertura,
+    int TempoDecorrido,
+    bool EstaConcluido
+);
+
+public record CadastrarChamadosViewModel(
+    [Required(ErrorMessage = "O campo \"Título\" deve ser preenchido.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "O campo \"Título\" deve conter entre 2 a 50 caracteres.")]
+    string Titulo,
+    [StringLength(500, ErrorMessage = "O campo \"Descrição\" deve conter no maximo 500 caracteres.")]
+    string? Descricao,
+
+    [Required(ErrorMessage = "O campo \"Equipamento\" deve ser preenchido.")]
+    string EquipamentoId
+);
+
+public record EditarChamadoViewModel(
+    string Id,
+    [Required(ErrorMessage = "O campo \"Título\" deve ser preenchido.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "O campo \"Título\" deve conter entre 2 a 50 caracteres.")]
+    string Titulo,
+    [StringLength(500, ErrorMessage = "O campo \"Descrição\" deve conter no maximo 500 caracteres.")]
+    string? Descricao,
+
+    [Required(ErrorMessage = "O campo \"Equipamento\" deve ser preenchido.")]
+    string EquipamentoId
+);
+
+public record ExcluirChamadoViewModel(
+    string Id,
+    string Titulo,
+    string? Descricao,
     string Equipamento,
     DateTime DataAbertura,
     int TempoDecorrido,
